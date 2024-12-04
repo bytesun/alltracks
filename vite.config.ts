e69e039e36f2c949106b9fb80b05e9deb3553f5c
Sun <vansdaynet@gmail.com>
2024-12-03 20:56:06 -0800

    rollback

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    headers: {
-      'Cache-Control': 'no-store',
-      'Pragma': 'no-cache',
-      'Expires': '0',
-    },
-  },
   build: {
     rollupOptions: {
       output: {
@@ -18,10 +11,7 @@ export default defineConfig({
           'arweave': ['arweave'],
           'react-vendor': ['react', 'react-dom'],
           'maps': ['leaflet', 'react-leaflet'],
-        },
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
+        }
       }
     },
     chunkSizeWarningLimit: 1000
