1ed138510721bf3763030dc675133b20726a3120
Sun <vansdaynet@gmail.com>
2024-12-05 17:23:09 -0800

    remove debug

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -347,7 +347,7 @@ function MainApp() {
 
   const recordPoint = async () => {
     setShowNotice(false);
-    setShowCommentModal(true);
+ 
     // showNotification('recordingMode:'+recordingMode, "info");
     if (recordingMode === 'manual') {
       if (navigator.geolocation) {
