593b1864d75654832ee49012f27b2865293c1b47
Sun <vansdaynet@gmail.com>
2025-10-22 18:20:14 -0700

    roll back ip

--- src/lib/canisters.ts
+++ src/lib/canisters.ts
@@ -3,7 +3,7 @@ import { HttpAgent } from "@dfinity/agent";
 export const HOST =
   "https://ic0.app";
 
-export const IDENTITY_PROVIDER = 'https://id.ai';//https://identity.ic0.app';
+export const IDENTITY_PROVIDER = https://identity.ic0.app';
 
 export const defaultAgent = new HttpAgent({
   host: HOST,
