6a70cee3af90a49fb47b88813d8e6f48230dd6d7
Sun <vansdaynet@gmail.com>
2026-09-02 19:04:37 -0700

    Simplify activity history page wrapper

--- src/pages/TracksPage.tsx
+++ src/pages/TracksPage.tsx
@@ -1,19 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useGlobalContext, useAlltracks } from '../components/Store';
-import { parseTracks } from '../utils/trackUtils';
-import { Link, useParams } from 'react-router-dom';
-import { Tracks } from '../components/Tracks'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Tracks } from '../components/Tracks';
 import '../styles/TracksPage.css';
 
 export const TracksPage = () => {
   const { userId } = useParams();
-  const { state: { isAuthed, principal } } = useGlobalContext();
-  const alltracks = useAlltracks();
-  const [tracks, setTracks] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  
-  return (
-    <Tracks userId={userId}/>
-  );
+  return <Tracks userId={userId} />;
 };
