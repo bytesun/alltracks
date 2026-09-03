778e090b546776f3715439c10e86fc2381b1c092
Sun <vansdaynet@gmail.com>
2026-09-02 19:14:55 -0700

    Remove legacy History CSS override

--- src/pages/TracksPage.tsx
+++ src/pages/TracksPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Tracks } from '../components/Tracks';
-import '../styles/TracksPage.css';
 
 export const TracksPage = () => {
   const { userId } = useParams();
